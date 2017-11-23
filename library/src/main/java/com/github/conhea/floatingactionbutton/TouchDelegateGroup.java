50eb35ed688fcc5eea3ab5094cfed232353e8664
Thomas Schmidt <bohsen@gmail.com>
2017-11-23 07:55:11 +0100

    Fixed wrong indentation

--- library/src/main/java/com/github/conhea/floatingactionbutton/TouchDelegateGroup.java
+++ library/src/main/java/com/github/conhea/floatingactionbutton/TouchDelegateGroup.java
@@ -9,63 +9,63 @@ import android.view.View;
 import java.util.ArrayList;
 
 public class TouchDelegateGroup extends TouchDelegate {
-  private static final Rect USELESS_HACKY_RECT = new Rect();
-  private final ArrayList<TouchDelegate> mTouchDelegates = new ArrayList<TouchDelegate>();
-  private TouchDelegate mCurrentTouchDelegate;
-  private boolean mEnabled;
+    private static final Rect USELESS_HACKY_RECT = new Rect();
+    private final ArrayList<TouchDelegate> mTouchDelegates = new ArrayList<TouchDelegate>();
+    private TouchDelegate mCurrentTouchDelegate;
+    private boolean mEnabled;
 
-  TouchDelegateGroup(View uselessHackyView) {
-    super(USELESS_HACKY_RECT, uselessHackyView);
-  }
-
-  void addTouchDelegate(@NonNull TouchDelegate touchDelegate) {
-    mTouchDelegates.add(touchDelegate);
-  }
-
-  public void removeTouchDelegate(TouchDelegate touchDelegate) {
-    mTouchDelegates.remove(touchDelegate);
-    if (mCurrentTouchDelegate == touchDelegate) {
-      mCurrentTouchDelegate = null;
+    TouchDelegateGroup(View uselessHackyView) {
+        super(USELESS_HACKY_RECT, uselessHackyView);
     }
-  }
 
-  void clearTouchDelegates() {
-    mTouchDelegates.clear();
-    mCurrentTouchDelegate = null;
-  }
+    void addTouchDelegate(@NonNull TouchDelegate touchDelegate) {
+        mTouchDelegates.add(touchDelegate);
+    }
 
-  @Override
-  public boolean onTouchEvent(@NonNull MotionEvent event) {
-    if (!mEnabled) return false;
-
-    TouchDelegate delegate = null;
-
-    switch (event.getAction()) {
-    case MotionEvent.ACTION_DOWN:
-      for (int i = 0; i < mTouchDelegates.size(); i++) {
-        TouchDelegate touchDelegate = mTouchDelegates.get(i);
-        if (touchDelegate.onTouchEvent(event)) {
-          mCurrentTouchDelegate = touchDelegate;
-          return true;
+    public void removeTouchDelegate(TouchDelegate touchDelegate) {
+        mTouchDelegates.remove(touchDelegate);
+        if (mCurrentTouchDelegate == touchDelegate) {
+            mCurrentTouchDelegate = null;
         }
-      }
-      break;
-
-    case MotionEvent.ACTION_MOVE:
-      delegate = mCurrentTouchDelegate;
-      break;
-
-    case MotionEvent.ACTION_CANCEL:
-    case MotionEvent.ACTION_UP:
-      delegate = mCurrentTouchDelegate;
-      mCurrentTouchDelegate = null;
-      break;
     }
 
-    return delegate != null && delegate.onTouchEvent(event);
-  }
+    void clearTouchDelegates() {
+        mTouchDelegates.clear();
+        mCurrentTouchDelegate = null;
+    }
 
-  void setEnabled(boolean enabled) {
-    mEnabled = enabled;
-  }
+    @Override
+    public boolean onTouchEvent(@NonNull MotionEvent event) {
+        if (!mEnabled) return false;
+
+        TouchDelegate delegate = null;
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                for (int i = 0; i < mTouchDelegates.size(); i++) {
+                    TouchDelegate touchDelegate = mTouchDelegates.get(i);
+                    if (touchDelegate.onTouchEvent(event)) {
+                        mCurrentTouchDelegate = touchDelegate;
+                        return true;
+                    }
+                }
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                delegate = mCurrentTouchDelegate;
+                break;
+
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                delegate = mCurrentTouchDelegate;
+                mCurrentTouchDelegate = null;
+                break;
+        }
+
+        return delegate != null && delegate.onTouchEvent(event);
+    }
+
+    void setEnabled(boolean enabled) {
+        mEnabled = enabled;
+    }
 }
